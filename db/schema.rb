32cb48ce4cbb232aca9814906fb4e57c02db45b8
iwaki-toshiyuki <toytoy0714@icloud.com>
2021-10-13 05:28:21 +0900

    fix:schema.rbの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_12_173355) do
+ActiveRecord::Schema.define(version: 2021_10_12_194131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,8 +30,6 @@ ActiveRecord::Schema.define(version: 2021_10_12_173355) do
 
   create_table "similar_members", force: :cascade do |t|
     t.text "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "static_pages", force: :cascade do |t|

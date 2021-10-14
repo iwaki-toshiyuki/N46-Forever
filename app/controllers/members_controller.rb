f38022d851d3be331cb81d027419f8405c1850dc
iwaki-toshiyuki <toytoy0714@icloud.com>
2021-10-15 02:03:38 +0900

    結果画面の作成2

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,9 +2,7 @@ class MembersController < ApplicationController
   def index
     @member = Member.all
   end
-
   private
-
     def member_params
         params.require(:member).permit(:name, :cute, :singing, :dance, :variety, :acting)
     end

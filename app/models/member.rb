e0aa91db7e819c6c9b24b68a53ade01b0258a34d
iwaki-toshiyuki <toytoy0714@icloud.com>
2021-10-25 13:30:02 +0900

    実験

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,18 +1,6 @@
 class Member < ApplicationRecord
   belongs_to:similar_member, optional: true
-  def self.chart_date1
-    order(cute: :desc).limit(5).pluck('name','cute').to_h
-  end
-  def self.chart_date2
-    order(singing: :desc).limit(5).pluck('name','singing').to_h
-  end
-  def self.chart_date3
-    order(dance: :asc).limit(5).pluck('name','dance').to_h
-  end
-  def self.chart_date4
-    order(variety: :asc).limit(5).pluck('name','variety').to_h
-  end
-  def self.chart_date5
-    order(acting: :desc).limit(5).pluck('name','acting').to_h
+  def self.chart_date
+    Member.select('cute,singing,dance,variety,acting').find(2)
   end
 end

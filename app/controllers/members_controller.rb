76c9242e4ede2f34afd20ed50bc36defb5cbb460
iwaki-toshiyuki <toytoy0714@icloud.com>
2021-10-28 23:37:17 +0900

    メンバーの割り振り

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,9 +1,164 @@
 class MembersController < ApplicationController
-  def index
+  def akimoto
     @member = Member.all
   end
-  private
-    def member_params
-        params.require(:member).permit(:name, :cute, :singing, :dance, :variety, :acting)
-    end
+
+  def endo
+    @member = Member.all
+  end
+
+  def hayakawa
+    @member = Member.all
+  end
+
+  def hayashi
+    @member = Member.all
+  end
+
+  def higuchi
+    @member = Member.all
+  end
+
+  def hoshino
+    @member = Member.all
+  end
+
+  def ikuta
+    @member = Member.all
+  end
+
+  def ito
+    @member = Member.all
+  end
+
+  def iwamoto
+    @member = Member.all
+  end
+
+  def kakehashi
+    @member = Member.all
+  end
+
+  def kaki
+    @member = Member.all
+  end
+
+  def kanagawa
+    @member = Member.all
+  end
+
+  def kitagawa
+    @member = Member.all
+  end
+
+  def kitano
+    @member = Member.all
+  end
+
+  def kubo
+    @member = Member.all
+  end
+
+  def kuromi
+    @member = Member.all
+  end
+
+  def matsuo
+    @member = Member.all
+  end
+
+  def mukai
+    @member = Member.all
+  end
+
+  def nakamura
+    @member = Member.all
+  end
+
+  def saito
+    @member = Member.all
+  end
+
+  def sakaguchi
+    @member = Member.all
+  end
+
+  def satoka
+    @member = Member.all
+  end
+
+  def satori
+    @member = Member.all
+  end
+
+  def seimiya
+    @member = Member.all
+  end
+
+  def sibata
+    @member = Member.all
+  end
+
+  def sinuchi
+    @member = Member.all
+  end
+
+  def suzuki
+    @member = Member.all
+  end
+
+  def takayama
+    @member = Member.all
+  end
+
+  def tamura
+    @member = Member.all
+  end
+
+  def terada
+    @member = Member.all
+  end
+
+  def tsutsui
+    @member = Member.all
+  end
+
+  def umezawa
+    @member = Member.all
+  end
+
+  def wada
+    @member = Member.all
+  end
+
+  def yakubo
+    @member = Member.all
+  end
+
+  def yamashita
+    @member = Member.all
+  end
+
+  def yamazaki
+    @member = Member.all
+  end
+
+  def yoda
+    @member = Member.all
+  end
+
+  def yoshida
+    @member = Member.all
+  end
+
+  def yumiki
+    @member = Member.all
+  end
+
+private
+
+  def member_params
+    params.require(:member).permit(:name, :cute, :singing, :dance, :variety, :acting)
+  end
+
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,6 +1,159 @@
 class Member < ApplicationRecord
   belongs_to:similar_member, optional: true
-  def self.chart_date
+  def self.chart_date1
+    Member.select('cute,singing,dance,variety,acting').find(1)
+  end
+
+  def self.chart_date2
     Member.select('cute,singing,dance,variety,acting').find(2)
   end
+
+  def self.chart_date3
+    Member.select('cute,singing,dance,variety,acting').find(3)
+  end
+
+  def self.chart_date4
+    Member.select('cute,singing,dance,variety,acting').find(4)
+  end
+
+  def self.chart_date5
+    Member.select('cute,singing,dance,variety,acting').find(5)
+  end
+
+  def self.chart_date6
+    Member.select('cute,singing,dance,variety,acting').find(6)
+  end
+
+  def self.chart_date7
+    Member.select('cute,singing,dance,variety,acting').find(7)
+  end
+
+  def self.chart_date8
+    Member.select('cute,singing,dance,variety,acting').find(8)
+  end
+
+  def self.chart_date9
+    Member.select('cute,singing,dance,variety,acting').find(9)
+  end
+
+  def self.chart_date10
+    Member.select('cute,singing,dance,variety,acting').find(10)
+  end
+
+  def self.chart_date11
+    Member.select('cute,singing,dance,variety,acting').find(11)
+  end
+
+  def self.chart_date12
+    Member.select('cute,singing,dance,variety,acting').find(12)
+  end
+
+  def self.chart_date13
+    Member.select('cute,singing,dance,variety,acting').find(13)
+  end
+
+  def self.chart_date14
+    Member.select('cute,singing,dance,variety,acting').find(14)
+  end
+
+  def self.chart_date15
+    Member.select('cute,singing,dance,variety,acting').find(15)
+  end
+
+  def self.chart_date16
+    Member.select('cute,singing,dance,variety,acting').find(16)
+  end
+
+  def self.chart_date17
+    Member.select('cute,singing,dance,variety,acting').find(17)
+  end
+
+  def self.chart_date18
+    Member.select('cute,singing,dance,variety,acting').find(18)
+  end
+
+  def self.chart_date19
+    Member.select('cute,singing,dance,variety,acting').find(19)
+  end
+
+  def self.chart_date20
+    Member.select('cute,singing,dance,variety,acting').find(20)
+  end
+
+  def self.chart_date21
+    Member.select('cute,singing,dance,variety,acting').find(21)
+  end
+
+  def self.chart_date22
+    Member.select('cute,singing,dance,variety,acting').find(22)
+  end
+
+  def self.chart_date23
+    Member.select('cute,singing,dance,variety,acting').find(23)
+  end
+
+  def self.chart_date24
+    Member.select('cute,singing,dance,variety,acting').find(24)
+  end
+
+  def self.chart_date25
+    Member.select('cute,singing,dance,variety,acting').find(25)
+  end
+
+  def self.chart_date26
+    Member.select('cute,singing,dance,variety,acting').find(26)
+  end
+
+  def self.chart_date27
+    Member.select('cute,singing,dance,variety,acting').find(27)
+  end
+
+  def self.chart_date28
+    Member.select('cute,singing,dance,variety,acting').find(28)
+  end
+
+  def self.chart_date29
+    Member.select('cute,singing,dance,variety,acting').find(29)
+  end
+
+  def self.chart_date30
+    Member.select('cute,singing,dance,variety,acting').find(30)
+  end
+
+  def self.chart_date31
+    Member.select('cute,singing,dance,variety,acting').find(31)
+  end
+
+  def self.chart_date32
+    Member.select('cute,singing,dance,variety,acting').find(32)
+  end
+
+  def self.chart_date33
+    Member.select('cute,singing,dance,variety,acting').find(33)
+  end
+
+  def self.chart_date34
+    Member.select('cute,singing,dance,variety,acting').find(34)
+  end
+
+  def self.chart_date35
+    Member.select('cute,singing,dance,variety,acting').find(35)
+  end
+
+  def self.chart_date36
+    Member.select('cute,singing,dance,variety,acting').find(36)
+  end
+
+  def self.chart_date37
+    Member.select('cute,singing,dance,variety,acting').find(37)
+  end
+
+  def self.chart_date38
+    Member.select('cute,singing,dance,variety,acting').find(38)
+  end
+
+  def self.chart_date39
+    Member.select('cute,singing,dance,variety,acting').find(39)
+  end
+
 end

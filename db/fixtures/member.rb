1ee8e62afe21150ab7c5d57078d26a236e266ba3
iwaki-toshiyuki <toytoy0714@icloud.com>
2021-10-31 00:13:07 +0900

    fix:seed_fuのデータを修正

--- db/fixtures/member.rb
+++ db/fixtures/member.rb
@@ -2,11 +2,11 @@ Member.seed do |s|
   s.id = 1
   s.name = "秋元真夏"
   s.profile = '1993-08-20'
-  s.cute = 85
-  s.singing = 35
-  s.dance = 55
-  s.variety = 100
-  s.acting =60
+  s.cute = 90
+  s.singing = 50
+  s.dance = 60
+  s.variety = 90
+  s.acting = 90
 end
 Member.seed do |s|
   s.id = 2
@@ -22,7 +22,7 @@ Member.seed do |s|
   s.id = 3
   s.name = "伊藤理々杏"
   s.profile = '2002-10-08'
-  s.cute = 75
+  s.cute = 100
   s.singing = 70
   s.dance = 80
   s.variety = 80
@@ -33,19 +33,19 @@ Member.seed do |s|
   s.name = "岩本蓮加"
   s.profile = '2004-02-02'
   s.cute = 90
-  s.singing = 75
-  s.dance = 85
-  s.variety = 75
-  s.acting = 75
+  s.singing = 90
+  s.dance = 90
+  s.variety = 90
+  s.acting = 90
 end
 Member.seed do |s|
   s.id = 5
   s.name = "梅澤美波"
   s.profile = '1999-01-06'
   s.cute = 70
-  s.singing = 70
-  s.dance = 80
-  s.variety = 85
+  s.singing = 85
+  s.dance = 90
+  s.variety = 90
   s.acting =80
 end
 Member.seed do |s|
@@ -53,10 +53,10 @@ Member.seed do |s|
   s.name = "遠藤さくら"
   s.profile = '2001-10-03'
   s.cute = 90
-  s.singing = 85
-  s.dance = 85
-  s.variety = 60
-  s.acting = 70
+  s.singing = 90
+  s.dance = 95
+  s.variety = 85
+  s.acting = 85
 end
 Member.seed do |s|
   s.id = 7
@@ -75,7 +75,7 @@ Member.seed do |s|
   s.cute = 90
   s.singing = 90
   s.dance = 80
-  s.variety = 80
+  s.variety = 90
   s.acting = 90
 end
 Member.seed do |s|
@@ -85,7 +85,7 @@ Member.seed do |s|
   s.cute = 75
   s.singing = 70
   s.dance = 100
-  s.variety = 100
+  s.variety = 95
   s.acting = 75
 end
 Member.seed do |s|
@@ -102,10 +102,10 @@ Member.seed do |s|
   s.id = 11
   s.name = "北野日奈子"
   s.profile = '1996-07-17'
-  s.cute = 75
+  s.cute = 90
   s.singing = 60
   s.dance = 75
-  s.variety = 80
+  s.variety = 90
   s.acting = 70
 end
 Member.seed do |s|
@@ -135,7 +135,7 @@ Member.seed do |s|
   s.cute = 90
   s.singing = 90
   s.dance = 95
-  s.variety = 85
+  s.variety = 90
   s.acting = 80
 end
 Member.seed do |s|
@@ -162,18 +162,18 @@ Member.seed do |s|
   s.id = 17
   s.name = "佐藤璃果"
   s.profile = '2001-08-09'
-  s.cute = 80
+  s.cute = 90
   s.singing = 80
   s.dance = 70
-  s.variety = 70
-  s.acting = 70
+  s.variety = 80
+  s.acting = 90
 end
 Member.seed do |s|
   s.id = 18
   s.name = "柴田柚菜"
   s.profile = '2003-03-03'
   s.cute = 85
-  s.singing = 90
+  s.singing = 100
   s.dance = 80
   s.variety = 75
   s.acting = 75
@@ -183,8 +183,8 @@ Member.seed do |s|
   s.name = "新内眞衣"
   s.profile = '1992-01-22'
   s.cute = 65
-  s.singing = 75
-  s.dance = 75
+  s.singing = 80
+  s.dance = 85
   s.variety = 95
   s.acting = 60
 end
@@ -192,9 +192,9 @@ Member.seed do |s|
   s.id = 20
   s.name = "鈴木絢音"
   s.profile = '1999-03-05'
-  s.cute = 75
+  s.cute = 80
   s.singing = 75
-  s.dance = 75
+  s.dance = 100
   s.variety = 70
   s.acting = 90
 end
@@ -202,7 +202,7 @@ Member.seed do |s|
   s.id = 21
   s.name = "清宮レイ"
   s.profile = '2003-08-01'
-  s.cute = 85
+  s.cute = 90
   s.singing = 90
   s.dance = 85
   s.variety = 90
@@ -213,9 +213,9 @@ Member.seed do |s|
   s.name = "高山一実"
   s.profile = '1994-02-08'
   s.cute = 80
-  s.singing = 90
-  s.dance = 85
-  s.variety = 90
+  s.singing = 95
+  s.dance = 90
+  s.variety = 95
   s.acting = 65
 end
 Member.seed do |s|
@@ -225,14 +225,14 @@ Member.seed do |s|
   s.cute = 90
   s.singing = 85
   s.dance = 85
-  s.variety = 80
-  s.acting = 85
+  s.variety = 95
+  s.acting = 90
 end
 Member.seed do |s|
   s.id = 24
   s.name = "筒井あやめ"
   s.profile = '2004-06-08'
-  s.cute = 85
+  s.cute = 100
   s.singing = 85
   s.dance = 80
   s.variety = 70
@@ -242,10 +242,10 @@ Member.seed do |s|
   s.id = 25
   s.name = "寺田蘭世"
   s.profile = '1998-09-23'
-  s.cute = 80
-  s.singing = 75
-  s.dance = 80
-  s.variety = 75
+  s.cute = 90
+  s.singing = 90
+  s.dance = 90
+  s.variety = 80
   s.acting = 85
 end
 Member.seed do |s|
@@ -266,7 +266,7 @@ Member.seed do |s|
   s.singing = 95
   s.dance = 80
   s.variety = 90
-  s.acting = 90
+  s.acting = 100
 end
 Member.seed do |s|
   s.id = 28
@@ -276,7 +276,7 @@ Member.seed do |s|
   s.singing = 90
   s.dance = 80
   s.variety = 85
-  s.acting = 85
+  s.acting = 95
 end
 Member.seed do |s|
   s.id = 29
@@ -286,13 +286,13 @@ Member.seed do |s|
   s.singing = 85
   s.dance = 90
   s.variety = 90
-  s.acting = 90
+  s.acting = 100
 end
 Member.seed do |s|
   s.id = 30
   s.name = "星野みなみ"
   s.profile = '1998-02-06'
-  s.cute = 90
+  s.cute = 100
   s.singing = 60
   s.dance = 75
   s.variety = 85
@@ -312,20 +312,20 @@ Member.seed do |s|
   s.id = 32
   s.name = "向井葉月"
   s.profile = '1999-08-23'
-  s.cute = 75
+  s.cute = 85
   s.singing = 80
   s.dance = 70
-  s.variety = 90
+  s.variety = 95
   s.acting = 70
 end
 Member.seed do |s|
   s.id = 33
   s.name = "矢久保美緒"
   s.profile = '2002-08-14'
-  s.cute = 70
+  s.cute = 85
   s.singing = 65
   s.dance = 70
-  s.variety = 85
+  s.variety = 95
   s.acting = 90
 end
 Member.seed do |s|
@@ -333,8 +333,8 @@ Member.seed do |s|
   s.name = "山崎怜奈"
   s.profile = '1997-05-21'
   s.cute = 70
-  s.singing = 70
-  s.dance = 70
+  s.singing = 80
+  s.dance = 85
   s.variety = 95
   s.acting = 75
 end
@@ -355,7 +355,7 @@ Member.seed do |s|
   s.cute = 75
   s.singing = 90
   s.dance = 90
-  s.variety = 90
+  s.variety = 100
   s.acting = 90
 end
 Member.seed do |s|
@@ -366,13 +366,13 @@ Member.seed do |s|
   s.singing = 70
   s.dance = 70
   s.variety = 60
-  s.acting = 85
+  s.acting = 95
 end
 Member.seed do |s|
   s.id = 38
   s.name = "与田祐希"
   s.profile = '2000-05-05'
-  s.cute = 90
+  s.cute = 100
   s.singing = 90
   s.dance = 85
   s.variety = 95

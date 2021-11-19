aa6408e1d5e6dbc2eed10d673b0926e2759d2cea
iwaki-toshiyuki <toytoy0714@icloud.com>
2021-11-19 23:42:27 +0900

    chart-js-railsを導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("chartkick").use(require("highcharts"))
-
+require("Chart.min")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference

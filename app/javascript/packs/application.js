2a801696990e7812918230dd74d340bebc3af068
iwaki-toshiyuki <toytoy0714@icloud.com>
2021-11-23 20:58:32 +0900

    import Chart from

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("chartkick").use(require("highcharts"))
-require("Chart.js")
-
+require("chart.js")
+import Chart from 'chart.js/auto';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)

10428f536d515c5b8e2f5260f98e4888a257f61c
iwaki-toshiyuki <toytoy0714@icloud.com>
2021-12-01 03:20:01 +0900

    Twitterアイキャチ画像の準備

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,34 @@
 module ApplicationHelper
+  def page_title(page_title = '')
+    base_title = 'N46-Forever'
+    page_title.empty? ? base_title : "#{page_title} | #{base_title}"
+  end
+
+  def default_meta_tags
+    {
+      title: 'N46-Forever',
+      reverse: true,
+      separator: '|',
+      description: '乃木坂46の次の推しメンを紹介するサービス',
+      keywords: '乃木坂46',
+      canonical: request.original_url,
+      noindex: !Rails.env.production?,
+      icon: [
+        { href: image_url('top.png') },
+        { href: image_url('top.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' }
+      ],
+      og: {
+        title: 'N46-Forever',
+        description: '乃木坂46の次の推しメンを紹介するサービス',
+        type: 'website',
+        url: request.original_url,
+        image: image_url('top.png'),
+        locale: 'ja_JP'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@Ws44olu6bygWDyB'
+      }
+    }
+  end
 end
